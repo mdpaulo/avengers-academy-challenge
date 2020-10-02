14a527b178f2c9a30c9d57008a194d70b27c9c1e
Paulo Meira <dev.ranma@outlookcom>
2020-10-02 11:25:23 -0300

    Adicionando comentario

--- aac-backend/app/src/entity/ocurrence/client/OccurrenceService.js
+++ aac-backend/app/src/entity/ocurrence/client/OccurrenceService.js
@@ -5,6 +5,7 @@ module.exports = function(application){
     ioClient.on("occurrence", function(ocurrenceNotify){
         console.info(ocurrenceNotify);
         console.info("Received new Occurrence!");        
+        /* Caso queira interromper a persistencia dos registros na base de dados basta comentar a linha a baixo*/
         application.app.src.entity.ocurrence.controllers.OccurrenceController.saveIncomingOccurrence(application,ocurrenceNotify);
     });
 }
