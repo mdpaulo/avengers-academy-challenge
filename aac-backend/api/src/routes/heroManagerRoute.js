1e077312ec1dfb7e380a254bbf4d9cc124cb2d7a
Paulo Meira <dev.ranma@outlookcom>
2020-10-02 02:10:07 -0300

    Alterações na estrutura de pasta do projeto

--- aac-backend/api/src/routes/heroManagerRoute.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(application){
-    application.get('/hero/findall', function(request,response){
-        application.api.src.controllers.heroManagerController.getall(application,request,response);
-    });
-
-    application.post('/hero/save', function(request, response){
-        application.api.src.controllers.heroManagerController.saveHero(application, request, response);
-    });
-}
--- aac-backend/api/src/routes/indexRoute.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(application){
-    application.get('/', function(request,response){
-        response.status(200).send({
-            sucess:'true',
-            message:'Service is Running'
-        });
-    });
-};
