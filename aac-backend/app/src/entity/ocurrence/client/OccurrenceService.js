6eb869aa013937ad04a11354e23b0b89fcc20fc8
Paulo Meira <dev.ranma@outlookcom>
2020-10-02 11:23:55 -0300

    liberando chamada de serviço

--- aac-backend/app/src/entity/ocurrence/client/OccurrenceService.js
+++ aac-backend/app/src/entity/ocurrence/client/OccurrenceService.js
@@ -5,6 +5,6 @@ module.exports = function(application){
     ioClient.on("occurrence", function(ocurrenceNotify){
         console.info(ocurrenceNotify);
         console.info("Received new Occurrence!");        
-        //application.app.src.entity.ocurrence.controllers.OccurrenceController.saveIncomingOccurrence(application,ocurrenceNotify);
+        application.app.src.entity.ocurrence.controllers.OccurrenceController.saveIncomingOccurrence(application,ocurrenceNotify);
     });
 }
