88ad8cdee1dc6fdbd8473495c35e98e365d0bf8c
Paulo Meira <dev.ranma@outlookcom>
2020-10-02 02:17:00 -0300

    Adição de mais tabelas

--- database/MySqlDatabaseInit.sql
+++ database/MySqlDatabaseInit.sql
@@ -25,4 +25,23 @@ longitude text NOT NULL,
 status varchar(100) NOT NULL
 );
 
-insert into avengers(codename,arank,latitude,longitude,status) values('Miss Marvel','B','-315613','-612365','AVAILABLE');
+insert into avengers(codename,arank,latitude,longitude,status) values('Goku','S','50.455738355108','-20.80019452816703','AVAILABLE');
+insert into avengers(codename,arank,latitude,longitude,status) values('Luffy','S','69.455738355108','-10.80065452965403','AVAILABLE');
+insert into avengers(codename,arank,latitude,longitude,status) values('Doutor Estranho','S','10.455738355108','-50.80035462816703','AVAILABLE');
+insert into avengers(codename,arank,latitude,longitude,status) values('Batman','A','09.498752455208','-90.12310162895103','AVAILABLE');
+insert into avengers(codename,arank,latitude,longitude,status) values('Wolverine','A','30.411738355208','-11.79410162816703','AVAILABLE');
+insert into avengers(codename,arank,latitude,longitude,status) values('Deadpool','B','-48.30082981543115','160.27333565349713','AVAILABLE');
+insert into avengers(codename,arank,latitude,longitude,status) values('Falcão','C','-22.42045526449409','102.42962452766108','AVAILABLE');
+insert into avengers(codename,arank,latitude,longitude,status) values('Gavião Arqueiro','C','-22.42045526449409','102.42962452766108','AVAILABLE');
+
+
+create table occurrence(
+id bigint NOT NULL PRIMARY KEY AUTO_INCREMENT,
+latitude text NOT NULL,
+longitude text NOT NULL,
+dangerlevel varchar(100) NOT NULL,
+monstername text NOT NULL,
+status varchar(100) default 'ASSAULT',
+creationdate timestamp default current_timestamp
+);
+
